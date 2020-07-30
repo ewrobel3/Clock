c74f083b96388ad1b5df1f857cdb188706a1b825
Erin Wrobel <ewrobel3@gatech.edu>
2020-07-30 14:53:06 -0400

    added digital clock interface

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ function moveHands() {
     var h = 30 * ((date.getHours() % 12) + date.getMinutes() / 60);
     var m = 6 * date.getMinutes();
     var s = 6 * date.getSeconds();
+
+    var textTime = "";
+    if(date.getHours() < 10) {
+        textTime += "0";
+    }
+    textTime += `${date.getHours()}:`;
+    if(date.getMinutes() < 10) {
+        textTime += "0";
+    }
+    textTime += `${date.getMinutes()}:`;
+    if(date.getSeconds() < 10) {
+        textTime += "0";
+    }
+    textTime += `${date.getSeconds()}`;
+
     document.getElementById("seconds").style.cssText = "-webkit-transform:rotate(" + s + "deg);";
     document.getElementById("minutes").style.cssText = "-webkit-transform:rotate(" + m + "deg);";
     document.getElementById("hours").style.cssText = "-webkit-transform:rotate(" + h + "deg);";
-
+    document.getElementById("digTime").innerHTML = textTime;
     setTimeout(moveHands, 1000);
 }
 
